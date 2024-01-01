814bf99e25ed6cd262195b436aaeb05c8885a0b5
Mohammad Ali <mdali89a@gmail.com>
2024-01-01 14:50:49 +0600

    Send mail Notification Register User

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -6,10 +6,13 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\LoginRequest;
 use App\Http\Requests\Api\V1\RegisterRequest;
 use App\Http\Resources\V1\UserResource;
+use App\Jobs\RegisterUserEmailJob;
 use App\Models\User;
+use App\Notifications\RegisterNotification;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 use Tymon\JWTAuth\Facades\JWTAuth;
@@ -27,6 +30,7 @@ class AuthController extends Controller
             $data = $request->only(['name', 'email']);
             $data['password'] = bcrypt($request->password);
             $user = User::create($data);
+            dispatch(new RegisterUserEmailJob($user));
             $data = [
                 'userData' => new UserResource($user)
             ];
